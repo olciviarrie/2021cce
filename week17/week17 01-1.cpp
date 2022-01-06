da16b47503c0fd3617fde82a077f04786a99be70
申凱菁 <91250041+olciviarrie@users.noreply.github.com>
2022-01-06 16:32:10 +0800

    Add files via upload

---
+++ week17/week17 01-1.cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int main()
+{
+    int n=1234567892;///Q;计琌ぶ
+    /// n%10 緇计,碞琌计
+    /// n/10 埃10,玡计
+    printf("%d => %d %d\n",n,n/10,n%10);
+    n=n/10;
+    printf("%d => %d %d\n",n,n/10,n%10);
+    n=n/10;
+    printf("%d => %d %d\n",n,n/10,n%10);
+    n=n/10;
+    printf("%d => %d %d\n",n,n/10,n%10);
+    n=n/10;
+    printf("%d => %d %d\n",n,n/10,n%10);
+    n=n/10;
+    printf("%d => %d %d\n",n,n/10,n%10);
+    n=n/10;
+    printf("%d => %d %d\n",n,n/10,n%10);
+    n=n/10;
+    printf("%d => %d %d\n",n,n/10,n%10);
+    n=n/10;
+    printf("%d => %d %d\n",n,n/10,n%10);
+    n=n/10;
+    printf("%d => %d %d\n",n,n/10,n%10);
+}
+
---
+++ week17/week17 02-1.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    int n=1234567892;///Q;计琌ぶ
+    /// n%10 緇计,碞琌计
+    /// n/10 埃10,玡计
+    int sum=0;
+    while(n>0)
+    {
+        printf("%d => %d %d\n",n,n/10,n%10);
+        sum+=n%10;
+
+        n=n/10;
+    }
+    printf("%d",sum);
+}
+
